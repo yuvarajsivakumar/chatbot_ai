b8aa3bcca3a70b8d98e6241f04edb60469ac3954
Yuvaraj S <126080528+yuvarajsivakumar@users.noreply.github.com>
2025-03-02 22:18:37 +0530

    chatbot_final_code.py

--- chatbot_final_code.py
+++ chatbot_final_code.py
@@ -90,7 +90,9 @@ model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]), activation='softmax'))
 
 # Compile model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
-sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+from tensorflow.keras.optimizers import SGD
+sgd = SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
+
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
 #fitting and saving the model 
